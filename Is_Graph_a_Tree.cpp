68ac450a1d8d9ccdd1356b00bfa504a242d1f690
Divye30 <54054987+Divye30@users.noreply.github.com>
2021-07-18 23:58:56 +0530

    Create Is_Graph_a_Tree.cpp

---
+++ Is_Graph_a_Tree.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<list>
+#include<queue>
+using namespace std;
+class Graph{
+    list<int> *l;
+    int V;
+public:
+    Graph(int V)
+    {
+        this->V=V;
+        l=new list<int>[V];
+    }
+    void AddEdge(int x,int y)
+    {
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
+    bool isTree()
+    {
+        bool *visited=new bool[V];
+        int *parent=new int[V];
+        queue<int> q;
+        for(int i=0; i<V; i++)
+        {
+            visited[i]=false;
+            parent[i]=i;
+        }
+        int src=0;
+        q.push(src);
+        visited[src]=true;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            for(int nbr:l[node])
+            {
+                if(visited[nbr]==true and parent[node]!=nbr)
+                    return false;
+                else if(!visited[nbr])
+                {
+                    visited[nbr]=true;
+                    parent[nbr]=node;
+                    q.push(nbr);
+                }
+            }
+        }
+        return true;
+    }
+};
+int main()
+{
+    Graph G(4);
+    G.AddEdge(0, 1);
+    G.AddEdge(3, 2);
+    G.AddEdge(1, 2);
+    G.AddEdge(0, 3);
+    if(G.isTree())
+        cout<<"It is a tree"<<endl;
+    else
+        cout<<"It is not a tree"<<endl;
+}
+
