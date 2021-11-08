b036707c431529ddf9b16a6556468679c12d8fbe
Divye30 <54054987+Divye30@users.noreply.github.com>
2021-11-08 23:20:17 +0530

    Create Unique_BSTs.cpp

---
+++ Unique_BSTs.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int uniqueBST(int n)
+{
+    vector<int> dp(n+1);
+    dp[0]= 1, dp[1]= 1;
+    for(int i= 2; i<= n; i++)
+        for(int j= 1; j<= i; j++)
+            dp[i]+= dp[i-j]*dp[j-1];
+    return dp[n];
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<uniqueBST(n)<<endl;
+}
